f7ebfee6e27f481121442d72d006db3d649d7330
Mumuji2008 <solvebirb@gmail.com>
2022-09-07 19:00:01 +0800

    j

--- learn/index.js
+++ learn/index.js
@@ -1,4 +1,19 @@
+
+
 window.onload = async ()=>{
+    document.getElementById("page").style.display="none";
+    v = setTimeout(()=>{
+        document.getElementById("loader").style.display="none";
+        document.getElementById("page").style.display = "";
+    },1000)
+    
+    document.getElementById("sidenav").style.width = 0;
+        items = document.getElementsByClassName("menuitem");
+        Array.from(items).forEach((i)=>{
+            i.style.display ="none";
+    })
+
+
     document.getElementById("hidemenu").addEventListener("click", function(e){
         document.getElementById("sidenav").style.width = 0;
         items = document.getElementsByClassName("menuitem");
@@ -8,9 +23,13 @@ window.onload = async ()=>{
     })
     document.getElementById("open").addEventListener("click",  function(e){
         document.getElementById("sidenav").style.width = "100px";
+        
         items = document.getElementsByClassName("menuitem");
         Array.from(items).forEach((i)=>{
             i.style.display ="";
         })
+    })
+    document.getElementById("learn").addEventListener("click",(e)=>{
+        document.getElementById("learncontainer").style.display = "";
     })
 }
